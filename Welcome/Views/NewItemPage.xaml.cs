6e124a0824a71b8460fa171b011ad0a1a0e9460f
Alrampaz <alrami11@asu.edu>
2021-10-27 13:01:45 -0400

    Add project files.

---
+++ Welcome/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Welcome.Models;
+using Welcome.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Welcome.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
