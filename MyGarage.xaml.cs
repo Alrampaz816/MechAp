e996ef9810bc8e2eca5f93d1dab32bc95c6c8081
sramosli <92879816+sramosli@users.noreply.github.com>
2021-11-22 18:36:58 -0700

    Add files via upload

---
+++ MyGarage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System.Windows.Input;
+
+namespace MechAp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyGarage : ContentPage
+    {
+
+        public MyGarage()
+        {
+            InitializeComponent();
+        }
+
+        private void VehicleRegBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new VehicleRegistration();
+        }
+
+        private void MainInputPgBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MaintenanceInput();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            App.Current.MainPage = new VehicleRegistration();
+            App.Current.MainPage = new MaintenanceInput();
+            return base.OnBackButtonPressed();
+        }
+    }
+}
---
+++ VehicleRegistration.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System.Windows.Input;
+
+namespace MechAp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VehicleRegistration : ContentPage
+    {
+
+        public VehicleRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void MyGarageBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MyGarage();
+        }
+
+        private void MaintenceInputBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MaintenanceInput();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            App.Current.MainPage = new MyGarage();
+            App.Current.MainPage = new MaintenanceInput();
+            return base.OnBackButtonPressed();
+        }
+    }
+}
