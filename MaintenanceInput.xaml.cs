610aefe352ecd4442e994cfd8f009ab3cf9e7c27
sramosli <92879816+sramosli@users.noreply.github.com>
2021-11-22 18:39:39 -0700

    Add files via upload

---
+++ MaintenanceInput.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System.Windows.Input;
+
+namespace MechAp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MaintenanceInput : ContentPage
+    {
+
+        public MaintenanceInput()
+        {
+            InitializeComponent();
+        }
+
+        private void MyGarageBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MyGarage();
+        }
+
+        private void VehicleRegBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new VehicleRegistration();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            App.Current.MainPage = new MyGarage();
+            App.Current.MainPage = new VehicleRegistration();
+            return base.OnBackButtonPressed();
+        }
+
+    }
+}
